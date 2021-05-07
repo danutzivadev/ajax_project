453aa3ff211d6a1643588411a8908cd8d769672c
dnz <danut.ivanescu@outlook.com>
2021-05-07 17:16:03 +0300

    test

--- js/widget.js
+++ js/widget.js
@@ -1,10 +1,22 @@
-var rq = new XMLHttpRequest();
-rq.onreadystatechange = function () {
-    if (rq.readyState === 4) {
-        var e = JSON.parse(rq.responseText);
-        console.log(e);
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function () {
+    if (xhr.readyState === 4) {
+        var employees = JSON.parse(xhr.responseText);
+        var statusHTML = '<ul class="bulleted">';
+        for (var i=0; i<employees.length; i += 1) {
+            if (employees[i].inoffice === true) {
+                statusHTML += '<li class="in">';
+        } else {
+                statusHTML += '<li class="out">';
+            }
+            statusHTML += employees[i].name;
+            statusHTML += '</li>';
+
+        }
+        statusHTML += '</ul>';
+        document.getElementById('employeeList').innerHTML = statusHTML;
     }
 };
 
-rq.open('GET', 'data/employees.json');
-rq.send()
+xhr.open('GET', 'data/employees.json');
+xhr.send();
